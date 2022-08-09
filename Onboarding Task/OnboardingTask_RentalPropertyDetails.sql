9ec5c3764ea1b2cd387930f9650398bcc721c2eb
aj-menon <menon.anjali11@gmail.com>
2022-08-09 13:44:19 +1200

    answers to questions 5 and 6

--- Onboarding Task/OnboardingTask_RentalPropertyDetails.sql
+++ Onboarding Task/OnboardingTask_RentalPropertyDetails.sql
@@ -1,6 +1,7 @@
 -- 1. All PropertyIDs and PropertyNames for Owner 1426
 
-SELECT p.Id AS PropertyID, p.Name AS PropertyName
+SELECT 
+	p.Id AS PropertyID, p.Name AS PropertyName
 FROM Property p
 LEFT JOIN OwnerProperty op ON 
 	p.Id=op.PropertyId 
@@ -11,30 +12,32 @@ WHERE
 
 -- 2. Current home value for the properties for Owner 1426.
 
-SELECT p.Id AS PropertyID, p.Name AS PropertyName, hv.Value AS CurrentHomeValue
+SELECT
+	p.Id AS PropertyID, p.Name AS PropertyName, hv.Value AS CurrentHomeValue
 FROM Property p
 LEFT JOIN OwnerProperty op ON 
 	p.Id = op.PropertyId
 LEFT JOIN PropertyHomeValue hv ON
-	op.PropertyId = hv.PropertyId AND 
-	hv.IsActive =1 AND 
-	hv.HomeValueTypeId = 1  -- HomeValueType = Current 
+	op.PropertyId = hv.PropertyId 
 WHERE 
-	op.OwnerId = 1426;
+	op.OwnerId = 1426 AND 
+	hv.IsActive =1 AND 
+	hv.HomeValueTypeId = 1  -- HomeValueType = Current ;
 	 
 
 -- 3. 
-SELECT p.Id AS PropertyID, p.Name AS PropertyName, hv.Value AS CurrentHomeValue,tp.PaymentAmount,tp.PaymentFrequencyId ,
-		CAST(tp.StartDate AS date) AS TenancyStartDate, CAST(tp.EndDate AS date) TenancyEndDate, 
-		CASE 
-			WHEN tp.PaymentFrequencyId = 1 -- Weekly
-				THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate))
-			WHEN tp.PaymentFrequencyId = 2 --Fortnightly 
-				THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate)/2) 
-			WHEN tp.PaymentFrequencyId = 3 --Monthly
-				THEN (tp.PaymentAmount * (DATEDIFF(Month, tp.StartDate ,tp.EndDate)+1))
-			ELSE 0 
-		END AS TotalRent
+SELECT 
+	p.Id AS PropertyID, p.Name AS PropertyName, hv.Value AS CurrentHomeValue,
+	CAST(tp.StartDate AS date) AS TenancyStartDate, CAST(tp.EndDate AS date) TenancyEndDate, 
+	CASE 
+		WHEN tp.PaymentFrequencyId = 1 -- Weekly
+			THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate))
+		WHEN tp.PaymentFrequencyId = 2 --Fortnightly 
+			THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate)/2) 
+		WHEN tp.PaymentFrequencyId = 3 --Monthly
+			THEN (tp.PaymentAmount * (DATEDIFF(Month, tp.StartDate ,tp.EndDate)+1))
+		ELSE 0 
+	END AS TotalRent
 FROM Property p
 LEFT JOIN PropertyHomeValue hv ON
 	p.Id = hv.PropertyId AND 
@@ -43,13 +46,17 @@ LEFT JOIN PropertyHomeValue hv ON
 LEFT JOIN TenantProperty tp ON
 	p.id = tp.PropertyId
 WHERE 
-	tp.PropertyID IN ('5597','5637','5638');  
+	tp.PropertyID IN ('5597','5637','5638') 
+ORDER BY 
+	TotalRent DESC
+	
 
 
 
 -- 4. Display the yield
 
-SELECT p.Id AS PropertyID, p.Name AS PropertyName,  pf.Yield AS PropertyYield
+SELECT 
+	p.Id AS PropertyID, p.Name AS PropertyName,  pf.Yield AS PropertyYield
 FROM Property p
 LEFT JOIN PropertyHomeValue hv ON
 	p.Id = hv.PropertyId AND 
@@ -61,6 +68,48 @@ LEFT JOIN PropertyFinance pf ON
 	p.Id = pf.PropertyId
 WHERE 
 	tp.PropertyId IN ('5597','5637','5638')  
+ORDER BY 
+	pf.Yield DESC
+
+
+
+-- 5. Display all available jobs
+
+SELECT 
+	j.Id AS JobId, j.JobDescription, s.Status 
+FROM Job j
+LEFT JOIN JobStatus s ON
+	j.JobStatusId = s.Id
+LEFT JOIN JobMedia jm ON 
+	jm.JobId = j.Id 
+WHERE 
+	jm.IsActive =0
+ORDER BY 
+	s.Status
+
+--6. Display all property names, tenant first and last name, rental payments per week/fortnight/month  
+--   for propeties owned by Owner 1426
+
+SELECT 
+	p.Id as PropertyID, p.Name as PropertyName, 
+	CONCAT(pr.FirstName,' ', pr.LastName) as CurrentTenantName,
+	trt.Name PaymentFrequency, tp.PaymentAmount 
+FROM Property p
+LEFT JOIN TenantProperty tp ON
+	p.id = tp.PropertyId
+LEFT JOIN PropertyRentalPayment prp ON
+	tp.PaymentFrequencyId = prp.Id
+LEFT JOIN TargetRentType trt on 
+	tp.PaymentFrequencyId = trt.Id
+LEFT JOIN Tenant t ON
+	t.Id = tp.TenantId AND t.IsActive = 1
+LEFT JOIN Person pr ON
+	pr.Id = t.Id
+WHERE 
+	tp.PropertyId IN ('5597','5637','5638'); 
+	
+
+
 
 
 
