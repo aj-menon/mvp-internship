5182137e0e7986f0ebf0393615cb6a03df85eebd
aj-menon <menon.anjali11@gmail.com>
2022-08-10 11:38:22 +1200

    update queries

--- Onboarding Task/OnboardingTask_RentalPropertyDetails.sql
+++ Onboarding Task/OnboardingTask_RentalPropertyDetails.sql
@@ -8,8 +8,6 @@ LEFT JOIN OwnerProperty op ON
 WHERE 
 	op.OwnerId = 1426;
 
-
-
 -- 2. Current home value for the properties for Owner 1426.
 
 SELECT
@@ -20,9 +18,11 @@ LEFT JOIN OwnerProperty op ON
 LEFT JOIN PropertyHomeValue hv ON
 	op.PropertyId = hv.PropertyId 
 WHERE 
-	p.Id IN ('5597','5637','5638')  AND 
+	op.OwnerId = 1426  AND 
 	hv.IsActive =1 AND 
-	hv.HomeValueTypeId = 1  -- HomeValueType = Current ;
+	hv.HomeValueTypeId = 1  -- HomeValueType = Current 
+ORDER BY
+	hv.Value DESC
 	 
 
 -- 3. 
@@ -31,14 +31,16 @@ SELECT
 	CAST(tp.StartDate AS date) AS TenancyStartDate, CAST(tp.EndDate AS date) TenancyEndDate, 
 	CASE 
 		WHEN tp.PaymentFrequencyId = 1 -- Weekly
-			THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate))
+			THEN CONVERT(DECIMAL(10,2),(tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate)))
 		WHEN tp.PaymentFrequencyId = 2 --Fortnightly 
-			THEN (tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate)/2) 
+			THEN CONVERT(DECIMAL(10,2),(tp.PaymentAmount * DATEDIFF(Week, tp.StartDate,tp.EndDate)/2))
 		WHEN tp.PaymentFrequencyId = 3 --Monthly
-			THEN (tp.PaymentAmount * (DATEDIFF(Month, tp.StartDate ,tp.EndDate)+1))
+			THEN CONVERT(DECIMAL(10,2),(tp.PaymentAmount * (DATEDIFF(Month, tp.StartDate ,tp.EndDate)+1)))
 		ELSE 0 
 	END AS TotalRent
 FROM Property p
+LEFT JOIN OwnerProperty op ON 
+	p.Id = op.PropertyId
 LEFT JOIN PropertyHomeValue hv ON
 	p.Id = hv.PropertyId AND 
 	hv.IsActive =1 AND 
@@ -46,7 +48,7 @@ LEFT JOIN PropertyHomeValue hv ON
 LEFT JOIN TenantProperty tp ON
 	p.id = tp.PropertyId
 WHERE 
-	p.Id IN ('5597','5637','5638') 
+	op.OwnerId = 1426 
 ORDER BY 
 	TotalRent DESC
 	
@@ -58,6 +60,8 @@ ORDER BY
 SELECT 
 	p.Id AS PropertyID, p.Name AS PropertyName,  pf.Yield AS PropertyYield
 FROM Property p
+LEFT JOIN OwnerProperty op ON 
+	p.Id = op.PropertyId
 LEFT JOIN PropertyHomeValue hv ON
 	p.Id = hv.PropertyId AND 
 	hv.IsActive =1 AND 
@@ -67,7 +71,7 @@ LEFT JOIN TenantProperty tp ON
 LEFT JOIN PropertyFinance pf ON
 	p.Id = pf.PropertyId
 WHERE 
-	tp.PropertyId IN ('5597','5637','5638')  
+	op.OwnerId = 1426  
 ORDER BY 
 	pf.Yield DESC
 
@@ -76,14 +80,12 @@ ORDER BY
 -- 5. Display all available jobs
 
 SELECT 
-	j.Id AS JobId, j.JobDescription, s.Status
+	 j.Id, j.JobDescription, s.Status, j.PercentDone
 FROM Job j
 LEFT JOIN JobStatus s ON
 	j.JobStatusId = s.Id
-LEFT JOIN JobMedia jm ON 
-	jm.JobId = j.Id 
 WHERE 
-	jm.IsActive =0
+	j.PercentDone = 0 AND S.Status IN ('Open', 'Pending')
 ORDER BY 
 	s.Status
 
@@ -95,18 +97,21 @@ SELECT
 	CONCAT(pr.FirstName,' ', pr.LastName) AS CurrentTenantName,
 	trt.Name AS PaymentFrequency, tp.PaymentAmount AS RentAmount
 FROM Property p
-LEFT JOIN TenantProperty tp ON
-	p.id = tp.PropertyId
+INNER JOIN OwnerProperty op ON 
+	p.Id = op.PropertyId
+INNER JOIN TenantProperty tp ON
+	p.id = tp.PropertyId AND
+	tp.IsActive = 1			-- tenant is current
 LEFT JOIN PropertyRentalPayment prp ON
 	tp.PaymentFrequencyId = prp.Id
 LEFT JOIN TargetRentType trt ON
 	tp.PaymentFrequencyId = trt.Id
-LEFT JOIN Tenant t ON
-	t.Id = tp.TenantId AND t.IsActive = 1
 LEFT JOIN Person pr ON
-	pr.Id = t.Id
+	pr.Id = tp.TenantId
 WHERE 
-	tp.PropertyId IN ('5597','5637','5638'); 
+	op.OwnerId = 1426
+ORDER BY
+	tp.PaymentAmount DESC; 
 	
 
 
